4050ab6cd987efa9497179c6a1f7bcbeeadb7f59
ermanzi <ermanzi@mit.edu>
2017-05-22 00:33:58 -0400

    smaller backup file

---
+++ README.md
@@ -0,0 +1,13 @@
+A place to type stuff every time you open a new tab
+TypeStuff gives you persistent textarea on which to write stuff every time you open a new tab. 
+- add links as in markdown: [example]{http://example.com}
+New:
+- Shorthand for lines: type "=" or "_" 4 times for line divider
+- Add or delete notes
+- Switch between different notes by clicking on tabs
+- Saves every 30 seconds. Hit "ctrl" to save immediately
+- Notes versioning. Go back in history to find something you deleted
+Todo:
+- op transforms for consistency across multiple tabs
+
+![Demo](http://i.imgur.com/UuhtCfu.png)
---
+++ index.js
@@ -0,0 +1,371 @@
+/**
+ * Created by ericmanzi on 1/23/16.
+ */
+// textarea = document.getElementById('content');
+textarea = document.getElementById('textarea-label');
+// textarea = document.body;
+hr = '_____________________________________________________________________________'
+hr2 = '==============================================================================='
+focusNodeId = 0;
+drawLineRegex = /^(_){3}$/g;
+linkRegex = /\[([^\[]+)\]\{([^\}]+)\}/g;
+linkRegexAnchor = /\[([^\[]+)\]\{([^\}]+)\}/g;
+selected_note = "stuff";
+selected_note_ref_id = 'content';
+dataPlaceholder = "type stuff... and add inline links as in markdown:"+
+" [an example]{https://example.com}";
+controlsTimeout = null
+
+function resizeBox() {
+    var windowWidth = window.innerWidth - 85;
+    Array.prototype.slice.call(document.getElementsByClassName('noteContent'),0).forEach((notediv)=>{
+        notediv.style.width = windowWidth;
+    });
+    // textarea.style.width = windowWidth;
+}
+
+function setZoom(currentZoom, mycallback) {
+    document.body.style.fontSize=currentZoom+"%";
+    chrome.storage.local.set({typedstuff_zoom: currentZoom}, function () {
+        //console.log("saved zoom:"+currentZoom);
+        if (mycallback) mycallback();
+    });
+}
+
+function replacer(match, p1, p2, p3, offset, string) {
+    return '<div contenteditable="false"><a href="'+p2+'">'+p1+'</a></div>&nbsp';
+}
+
+function replaceLink(content) {
+    var regex = /\[([^\[]+)\]\{([^\}]+)\}/g;
+    var new_string = content.replace(regex, replacer);
+    return new_string;
+}
+
+function registerNotesHandler() {
+
+    document.getElementById('new').onclick = function(e) {
+        var new_note = document.createElement('span')
+        new_note.className = "note"
+        // new_note.textContent = "untitled"
+        var noteId = (new Date()).getTime();
+        new_note.setAttribute('notename', 'untitled')
+        new_note.setAttribute('refnoteid', noteId)
+        new_note.setAttribute('contenteditable', 'true')
+        new_note.setAttribute('data-placeholder', 'new note')
+        e.target.parentNode.appendChild(new_note)
+        registerNotesHandler();
+
+        var newNoteDiv = document.createElement('div');
+        newNoteDiv.id = noteId;
+        newNoteDiv.setAttribute('contenteditable','true')
+        newNoteDiv.setAttribute('spellcheck','false')
+        newNoteDiv.setAttribute('data-placeholder',dataPlaceholder)
+        newNoteDiv.className = "noteContent"
+        newNoteDiv.style.display = "none"
+        document.getElementById('textarea-label').appendChild(newNoteDiv);
+
+    }
+
+
+    var notes = Array.prototype.slice.call(document.getElementsByClassName('note'), 0);
+    notes.forEach((n)=> {
+        if (n.id == "new") {
+            return;
+        }
+        
+
+        (function(note){
+            note.onclick = function() {
+                // unselect other cells
+                var all_notes = Array.prototype.slice.call(document.getElementsByClassName('note'), 0);
+                all_notes.forEach((nx)=>{ nx.className = 'note'; });
+                note.className = 'note selected';
+                selected_note = note.textContent;
+
+                var refNoteId = note.getAttribute('refnoteid');
+                var all_note_divs = Array.prototype.slice.call(document.getElementsByClassName('noteContent'), 0);
+                all_note_divs.forEach((nd)=> {
+                    nd.style.display = 'none';
+                });
+                document.getElementById(refNoteId).style.display="block";
+            }
+            note.ondblclick = function(e) {
+                note.setAttribute('contenteditable', 'true')
+            }
+            note.addEventListener('keydown', function(e) {
+                if (e.which === 13) { // on enter
+                    e.stopPropagation();
+                    e.preventDefault();
+                    note.setAttribute('contenteditable', 'false')
+                    note.setAttribute('notename', note.textContent)
+                    return false;
+                }
+            })
+        })(n);
+    });
+}
+
+function hideControls() {
+    document.getElementById('controls').className='';
+    controlsTimeout = setTimeout(()=>{
+        hideControls();
+    }, 20000)
+}
+
+(function() {
+
+    resizeBox();
+
+    window.onresize = resizeBox;
+
+    var currentZoom;
+
+    chrome.storage.local.get('typedstuff_style', function(data) {
+        document.body.className = data.typedstuff_style || 'terminal';
+        selected_note = data.typestuff_selected_note || 'stuff';
+        Array.prototype.slice.call(document.getElementsByTagName('button'),0)
+            .filter((function(x) {
+                return x.className === document.body.className;
+            }))[0].className+=" selected";
+
+
+        chrome.storage.local.get('typedstuff_zoom', function (data) {
+            currentZoom = data.typedstuff_zoom || 100;
+            setZoom(currentZoom, function() {
+
+                chrome.storage.local.get('typedstuff_text', function (data) {
+                    textarea.innerHTML = data.typedstuff_text || '';
+
+                    chrome.storage.local.get('typedstuff_last_save', function (data) {
+                        lastSave = data.typedstuff_last_save || (new Date()).getTime();
+
+                        chrome.storage.local.get('typedstuff_notes_nav', function (data) {
+                            if (data != undefined && data != null && data != "") {
+                                document.getElementById("notesNavigator").innerHTML = 
+                                    data.typedstuff_notes_nav;
+                                registerNotesHandler();
+                            }
+                                               
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    textarea.addEventListener('keydown', function(e) {
+        // console.log(e.which)
+        var anchor = window.getSelection().anchorNode;
+        
+        focusNodeId = (new Date()).getTime();
+        // window.getSelection().anchorNode.parentNode.id = focusNodeId;
+        var content = textarea.innerHTML;
+                
+        if (e.shiftKey) {
+            if (e.which == 221) {
+                console.log("keypress }")
+                // console.log(anchor.parentNode.previousSibling.textContent);
+                // console.log(anchor.data);
+                // console.log(anchor.parentNode);
+                if (linkRegexAnchor.test(anchor.data+"}")) {
+                    //console.log("found link");
+                    // setTimeout(()=>{
+                        // console.log(anchor.data);
+                        // console.log(anchor.parentNode);
+                        var newContent = replaceLink(anchor.data+"}");
+                        // console.log(newContent)
+                        anchor.parentNode.innerHTML = newContent;
+                        e.preventDefault();
+                        e.stopPropagation();
+                        // content = replaceLink(content);
+                        // textarea.innerHTML = content;
+                        // var el = document.getElementById(focusNodeId);
+                        // var range = document.createRange();
+                        // var sel = window.getSelection();
+                        // range.setStart(el.childNodes[0], 0);
+                        // range.collapse(true);
+                        // sel.removeAllRanges();
+                        // sel.addRange(range);
+                        // el.focus();
+
+                    // }, 1);
+                }
+                 /*else {
+                    var previous = anchor.parentNode.previousSibling;
+                    if (previous == null || previous.tagName !== 'SPAN') { return; }
+                    var withPreviousSpan = previous.textContent + anchor.data;
+                    if (linkRegex.test(withPreviousSpan)) {
+                        //console.log("found link");
+                        setTimeout(()=>{
+                            var newContent = replaceLink(withPreviousSpan);
+                            //console.log(newContent)
+                            anchor.parentNode.innerHTML = newContent;
+                        }, 0);
+                    }
+                }*/
+            }
+            if (e.which == 189) { // keypress "_"
+                if (anchor.data == '___') {
+                    setTimeout(() => { anchor.data = hr }, 0);
+                }
+                //if (drawLineRegex.test(content)) {
+                    //content = drawLine(content);
+                    //textarea.innerHTML = content;
+                    //document.getElementById(focusNodeId).focus();    
+                //}
+            }
+
+
+
+        }
+
+        if (e.which == 9) {
+            e.preventDefault();
+            e.stopPropagation();
+            // var s = "&nbsp;"
+            // var tab = s+s+s+s;
+            // anchor.data += tab;
+        }
+
+        if (e.which == 17) {
+            //console.log("ctrl");
+            chrome.storage.local.set({typedstuff_text: content}, function () {
+                console.log("saved content");
+                lastSave = (new Date()).getTime();
+                chrome.storage.local.set({typedstuff_last_save: lastSave}, function () {
+                    var notesNavigator = document.getElementById('notesNavigator');
+                        chrome.storage.local.set({typedstuff_notes_nav: notesNavigator.innerHTML}, function() {
+                            // console.log("saved notes: "+notesNavigator.innerHTML);
+                        })
+                });
+            });
+        }
+
+
+        if (e.which == 187) {
+            if (anchor.data == '===') {
+                setTimeout(() => { anchor.data = hr2 }, 0);
+            }
+        }
+        // if (e.which == 9) {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        //     anchor.data += "...";
+        // }
+
+        setTimeout(()=>{
+            var timeSinceLastSave = (new Date()).getTime() - Number.parseInt(lastSave);
+            if (timeSinceLastSave > (24*60*60*1000)) {
+                var d = ":::::::::::::::::::::::::::::::::::::::::::::::";
+                var d2 = d+d+d+d+d+d+d;
+                var delimiter = d2+d2+d2;
+                var new_content = content + delimiter;
+                chrome.storage.local.get('typedstuff_backup', function (data) {
+                    var backup = data.typedstuff_backup + new_content || new_content;
+                    chrome.storage.local.set({typedstuff_backup: backup}, function () {
+                        console.log("backed up");
+                    });
+
+                });
+
+
+                //var fs = require('fs');
+                //var new_filename = "/Users/ericmanzi/Desktop/project\ stuff/typestuff_files/"+focusNodeId+".txt";
+                //fs.appendFile(new_filename, content, (err) => {
+                    // if (err) throw err;
+                // });
+            } else if (timeSinceLastSave > 30000) {
+
+                chrome.storage.local.set({typedstuff_text: content}, function () {
+                    console.log("saved content");
+                    lastSave = (new Date()).getTime();
+                    chrome.storage.local.set({typedstuff_last_save: lastSave}, function () {
+                        var notesNavigator = document.getElementById('notesNavigator');
+                        chrome.storage.local.set({typedstuff_notes_nav: notesNavigator.innerHTML}, function() {
+                            // console.log("saved notes: "+notesNavigator);
+
+                        })
+                    });
+                });
+            }
+
+            
+        }, 1);
+    });
+
+    document.getElementById('bigger').onclick = function() {
+        currentZoom+=10;
+        setZoom(currentZoom);
+
+    };
+
+    document.getElementById('smaller').onclick = function() {
+        currentZoom-=10;
+        setZoom(currentZoom);
+    };
+
+    document.getElementById('settings').onclick = function() {
+        if (document.getElementById('controls').className==='show') {
+            document.getElementById('controls').className='';
+        } else {
+            clearTimeout(controlsTimeout);
+            hideControls();
+            document.getElementById('controls').className='show';
+        }
+    };
+
+
+    document.getElementById('delete').onclick = function(e) {
+        if (window.confirm("Are you sure you want to delete the current note '"+selected_note+"'?")) {
+            // delete note
+            var selectedNote = document.getElementsByClassName('note selected')[0];
+            var refNoteId = selectedNote.getAttribute('refnoteid');
+            var refNote = document.getElementById(refNoteId);
+
+            selectedNote.parentNode.removeChild(selectedNote);
+            refNote.parentNode.removeChild(refNote);
+
+        }
+    }
+
+
+
+    registerNotesHandler();
+
+    hideControls();
+
+    var buttons = Array.prototype.slice.call(document.getElementsByTagName('button'), 0);
+    for (var i=0; i<buttons.length; i++) {
+        (function(index){
+            buttons[index].onclick = function() {
+
+                document.body.className = buttons[index].className.split(' ').filter(
+                    function(classname) {
+                        return classname !== 'selected';
+                    }
+                )[0];
+
+                chrome.storage.local.set({
+                    typedstuff_style: buttons[index].className
+                }, function () {
+                    console.log("saved style");
+                });
+
+                for (var j=0; j<buttons.length; j++) {
+                    if (j!==index) {
+                        var new_classname = buttons[j].className.split(' ').filter(
+                            function(classname) {
+                                return classname !== 'selected';
+                            }
+                        )[0];
+                        buttons[j].className = new_classname;
+                    }
+                }
+
+                buttons[index].className += " selected";
+            }
+        })(i);
+    }
+
+})();
